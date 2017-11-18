f00f2e631926665115202593642948767640621a
Jorge Yago <jorge.yago.meniz@gmail.com>
2017-11-18 16:06:49 +0100

    Corregido

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,51 +1,99 @@
 $(function(){
+
+function reset () {
+  $(".btn").removeClass("active");
+  $(".btn-pepperoni").removeClass("active");
+  $(".crust").removeClass("crust-gluten-free");
+  $(".sauce").removeClass("sauce-white");
+  $(".pep").hide();
+  $(".mushroom").hide();
+  $(".green-pepper").hide();
+  $(".price ul li:contains($1 pepperonni)").hide();
+  $(".price ul li:contains($1 mushrooms)").hide();
+  $(".price ul li:contains($1 green peppers)").hide();
+  $(".price ul li:contains($3 white sauce)").hide();
+  $(".price ul li:contains($5 gluten-free crust)").hide();
+  $("strong").text("$10");
+}
+reset();
+
   $(".btn-pepperonni").click(function(){
     $(this).toggleClass("active");
 if ($(this).hasClass("active")){
   $(".pep").show();
+  $(".price ul li:contains($1 pepperonni)").show();
 } else {
   $(".pep").hide();
+  $(".price ul li:contains($1 pepperonni)").hide();
 }
+updatePrice();
   });
 
 $(".btn-mushrooms").click(function(){
     $(this).toggleClass("active");
 if ($(this).hasClass("active")){
   $(".mushroom").show();
+  $(".price ul li:contains($1 mushrooms)").show();
 } else {
   $(".mushroom").hide();
+  $(".price ul li:contains($1 mushrooms)").hide();
 }
+updatePrice();
   });
 
 $(".btn-green-peppers").click(function(){
       $(this).toggleClass("active");
   if ($(this).hasClass("active")){
     $(".green-pepper").show();
+    $(".price ul li:contains($1 green peppers)").show();
   } else {
     $(".green-pepper").hide();
+    $(".price ul li:contains($1 green peppers)").hide();
   }
+  updatePrice();
     });
 
-  $(".btn-sauce").text("Regular Sauce");
-  $(".btn-sauce").click(function(){
-    if ($(this).text("Regular Sauce")){
-      $(".sauce-white").hide();
-      $(this).text("White Sauce");
+  $(".btn-sauce").click(function() {
+    $(".sauce").toggleClass("sauce-white");
+    if($(this).hasClass("active")){
+      $(this).removeClass("active");
+      $(".price ul li:contains($3 white sauce)").hide();
     } else {
-      $(".sauce-white").show();
-      $(this).text("Regular Sauce");
+      $(this).addClass("active");
+      $(".price ul li:contains($3 white sauce)").show();
     }
-      });
+    updatePrice();
+  });
 
-  $(".btn-crust").text("Regular Crust");
-  $(".btn-crust").click(function(){
-    if ($(this).text("Regular Crust")){
-      $(".crust-gluten-free").hide();
-      $(this).text("Gluten Free Crust");
+  $(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
+    if($(this).hasClass("active")){
+      $(this).removeClass("active");
+      $(".price ul li:contains($5 gluten-free crust)").hide();
     } else {
-      $(".crust-gluten-free").show();
-      $(this).text("Regular Crust");
+      $(this).addClass("active");
+      $(".price ul li:contains($5 gluten-free crust)").show();
     }
-      });
+    updatePrice();
+  });
+
+// Calculo de precio total
+
+function updatePrice() {
+  var total = 10;
+  $(".price li").hide();
+
+  $(".btn.active").toArray().forEach(function(btn, index) {
+    var ingredient = $(btn).text().toLowerCase();
+    var $listElement = $("li:contains('" + ingredient + "')");
+    var ingredientPrice = Number(
+      $listElement.text().split(" ")[0].replace("$","")
+      );
+      total += ingredientPrice;
+      $listElement.show();
+    });
+  $(".price strong").text("$" + total);
+}
+
 
 });
